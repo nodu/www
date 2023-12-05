7f31bb4ca34815e68bfa4d99900dd4921a76d76a
Matt Nodurfth <mnodurft@gmail.com>
2023-12-04 16:40:10 -0800

    Fix jsx

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -43,13 +43,13 @@ export default function Modal({ openModal, closeModal, isOpen, title, body, butt
                         xmlns="http://www.w3.org/2000/svg"
                         fill="none"
                         viewBox="0 0 24 24"
-                        stroke-width="1.5"
+                        strokeWidth="1.5"
                         stroke="currentColor"
-                        class="h-6 w-6"
+                        className="h-6 w-6"
                       >
                         <path
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
                           d="M6 18L18 6M6 6l12 12"
                         />
                       </svg>
