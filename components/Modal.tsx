89627b7c3c1b434d29e6c8fbf844b16e38962e87
Matt Nodurfth <mnodurft@gmail.com>
2023-12-04 16:38:36 -0800

    Add close button

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -38,6 +38,22 @@ export default function Modal({ openModal, closeModal, isOpen, title, body, butt
                 <Dialog.Panel className="w-full max-w-2xl transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
                   <Dialog.Title as="h1" className="text-lg font-medium leading-6 text-gray-900">
                     {title}
+                    <button className="fixed right-10" onClick={closeModal}>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                        stroke="currentColor"
+                        class="h-6 w-6"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          d="M6 18L18 6M6 6l12 12"
+                        />
+                      </svg>
+                    </button>
                   </Dialog.Title>
                   <div className="mt-2">{body}</div>
                 </Dialog.Panel>
