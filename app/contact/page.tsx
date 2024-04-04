f4a581a3449cdc57997ab55c1eaca900c5b0f000
Matt Nodurfth <mnodurft@gmail.com>
2024-04-04 16:28:51 -0700

    Update page title

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,7 +1,7 @@
 import ContactUs from '@/components/ContactUs'
 import { genPageMetadata } from 'app/seo'
 
-export const metadata = genPageMetadata({ title: 'Consulting' })
+export const metadata = genPageMetadata({ title: 'Contact' })
 
 export default function Page() {
   return (
